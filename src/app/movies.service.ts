192868c03a2e1b908923a2724966a5a8cd5678dc
tamrat-bayeh <tamrat3@gmail.com>
2020-03-04 15:08:09 +0200

    form submit

--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -10,25 +10,22 @@ import { map } from 'rxjs/operators';
 })
 export class MoviesService {
 
-   apiKey = "d759a614b16c0c1c0295b4313e94aeec";
+  apiKey = "d759a614b16c0c1c0295b4313e94aeec";
+  
   constructor( private http: HttpClient ) { }
 
   getMoviesByName(inputText): Observable<Movie[]> {
-    const apiKey = "d759a614b16c0c1c0295b4313e94aeec";
-    //! complete the movie img
-    
-    return  this.http.get<{results: Movie[]}>(`https://api.themoviedb.org/3/search/multi?api_key=${this.apiKey}&query=${inputText}&language=en-US&page=1&include_adult=false`).pipe(
+    const moviesUrl = `https://api.themoviedb.org/3/search/multi?api_key=${this.apiKey}&query=${inputText}&language=en-US&page=1&include_adult=false`
+    return  this.http.get<{results: Movie[]}>(moviesUrl).pipe(
       map((result) => {
         return result.results 
       })
     );
-
     }
 
   getMoviesById(movie_id): Observable<Movie> {
-    return  this.http.get<Movie>(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=${this.apiKey}&language=en-US`);
+    const moviesUrl = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${this.apiKey}&language=en-US`
+    return  this.http.get<Movie>(moviesUrl);
     }
-
-  
 
 }
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../model/Movie';
+import { NgForm } from '@angular/forms';
 import { MoviesService } from '../movies.service';
 
 
@@ -23,29 +24,27 @@ export class MoviesComponent implements OnInit {
     return this.movieService.getMoviesByName(name)
     .subscribe(movies => {
       console.log('movies.results',movies);
-      
       this.movies = movies.filter(movie => movie.title)})
-    
-
   }
+
   onSubmit(): void{
     this.getMovieByName(this.movieName);
-    this.saveInHistory () 
+    this.saveInHistory();
   }
   saveInHistory () :void {
     let historyItems = localStorage.getItem('searchHistory');
     let date = new Date().toDateString();
     let toSave = {search:this.movieName, date : date}
+
     if (historyItems) {
       let data = JSON.parse(historyItems);
       data.push(toSave);
-      localStorage.setItem("searchHistory", JSON.stringify(data))
+      localStorage.setItem("searchHistory", JSON.stringify(data));
     }else{
-      let toSave =[ {search:this.movieName, date : date} ]
-      localStorage.setItem("searchHistory", JSON.stringify(toSave))
+      let toSave =[ {search:this.movieName, date : date} ];
+      localStorage.setItem("searchHistory", JSON.stringify(toSave));
     }
-
-    
+   
   }
 
 }
