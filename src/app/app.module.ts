169f87c28af45a6485c731788ceecd528368a8d3
tamrat-bayeh <tamrat3@gmail.com>
2020-03-04 11:36:12 +0200

    service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {  } from "module";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from "../model/Movie";
+import { MoviesService } from "../movies.service";
 
 @Component({
   selector: 'app-movies',
@@ -6,12 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies.component.scss']
 })
 export class MoviesComponent implements OnInit {
+  movies : Movie[] = [];
+  movieName : string;
 
-  constructor() { }
+  constructor( private movieService:MoviesService  ) { }
 
   ngOnInit(): void {
     
   }
 
+  // getMovieByName(name:string) {
+  //   return this.movieService.getMoviesByName(name)
+  //   .subscribe(movies => console.log(movies)
+  //   )
+  // }
+  // onSubmit():void {
+  //   console.log(this.movieName);
+    
+  // }
 
 }
