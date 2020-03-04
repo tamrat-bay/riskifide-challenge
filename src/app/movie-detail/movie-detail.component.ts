bbcb44f6fa53e48d20c1dd342f2b9bb1e933eaae
Shlomi Baruch <shlomibaruch5@gmail.com>
2020-03-04 14:52:11 +0200

    search movies

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
-import { Movie } from '../model/Movie';
-=======
 import { Component, OnInit , Input} from '@angular/core';
 import { Movie } from '../model/Movie';
 
->>>>>>> 9690aa3bd57a6114192598607841db84dc9cb3ad
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../model/Movie';
 import {MoviesService} from '../movies.service';
-
-
-
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
@@ -11,26 +8,22 @@ import {MoviesService} from '../movies.service';
 })
 export class MoviesComponent implements OnInit {
  
-  movieName:string;
-  movies:Movie[];
+  movieName: string;
+  movies: Movie[];
   constructor(private movieService: MoviesService) { }
-
   ngOnInit(): void {
-    this.onSubmit()
   }
-
-  getMovieByName(name:string){
+  getMovieByName(name: string){
     return this.movieService.getMoviesByName(name)
     .subscribe(movies => {
+      console.log('movies.results',movies.results);
 
-    });
-  
+      return this.movies = movies.results.filter(movie => movie.title)})
+
   }
-  onSubmit():void {
- 
+  onSubmit(): void {
+
     this.getMovieByName(this.movieName)
-    console.log(this.movies);
 
   }
-
 }
