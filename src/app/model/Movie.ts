0f5de7e7778dbe20d1d9b47408b33b67c4e21719
tamrat-bayeh <tamrat3@gmail.com>
2020-03-04 11:04:52 +0200

    service added

--- src/app/model/Movie.ts
+++ src/app/model/Movie.ts
@@ -1,7 +1,7 @@
 export interface Movie {
     id:  number;
-    title: string
-    release_date: string
-    overview: string 
-    poster: string
+    title: string;
+    release_date: string;
+    overview: string ;
+    poster: string;
 }
--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -15,7 +15,6 @@ export class MoviesService {
     const apiKey = "d759a614b16c0c1c0295b4313e94aeec";
     //! complete the movie img
     const imgUrl = "https://image.tmdb.org/t/p/w500";
-
     return  this.http.get<Movie[]>(`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${inputText}&language=en-US&page=1&include_adult=false`);
 
     }
